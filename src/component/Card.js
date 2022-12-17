327d32c4587efc5c427342c0a37c1af8688deeb9
Arpit kumar <rk9003278@gmail.com>
2022-12-17 23:08:15 +0530

    Changed Card Props

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({ title, image }) => {
   return (
     <div className='card'>
-    <div className="title">
-
-        <h5>{props.title}</h5>
-    </div>
-        <div className="image">
-            <img src={props.image} alt="image" />
-        </div>
+      <div className="title">
+        <h5>{title}</h5>
+      </div>
+      <div className="image">
+        <img src={image} alt="image" />
+      </div>
     </div>
   )
 }
